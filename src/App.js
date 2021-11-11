be00d06289ebccfa85fd7f1da601a39f29e28ca3
Vinicius Cerqueira Bonifacio <vinicius.bonifacio1985@gmail.com>
2021-11-11 21:21:51 +0200

    Initial settings, dotenv config, firebase / firestore config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [state, setState] = useState({});
+
+	const onChangeHandler = (e) => setState({ ...state, [e.target.name]: e.target.value });
+	const onClickHandler = (e) => {
+		e.preventDefault();
+		console.log(state);
+	};
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<h1>Getting Started with Firebase</h1>
+			</header>
+			<section>
+				<h2>Firebase Firestore</h2>
+				<form className="add">
+					<label htmlFor="name">Name:</label>
+					<input type="text" name="name" required onChange={(e) => onChangeHandler(e)} />
+					<label htmlFor="name">Appearance:</label>
+					<input type="text" name="appearance" required onChange={(e) => onChangeHandler(e)} />
+					<button onClick={(e) => onClickHandler(e)}>Add New Characters</button>
+				</form>
+			</section>
+		</div>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,43 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+
+const firebaseConfig = {
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID,
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+// Initialize Firebase Services
+const db = getFirestore();
+
+// Collection reference
+const charactersRef = collection(db, 'characters');
+
+// Getting the Collection Data
+getDocs(charactersRef)
+	.then((snapshot) => {
+		let characters = [];
+		snapshot.docs.forEach((doc) => {
+			characters.push({ ...doc.data(), id: doc.id });
+		});
+		console.log(characters);
+	})
+	.catch((error) => console.log(error));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
